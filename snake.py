03f139bc23374794eab3b77fe127570f873ba944
Netsfly <sasha.tsoi20@gmail.com>
2025-03-09 17:32:45 +0600

    class fruit

--- snake.py
+++ snake.py
@@ -1,4 +1,12 @@
 import pygame, sys
+from pygame.math import Vector2
+class FRUIT:
+    def __init__(self):
+        self.x = 5
+        self.y = 4
+        self.pos = Vector2(self.x,self.y)
+    
+    
 pygame.init()
 screen = pygame.display.set_mode((400,500))
 clock = pygame.time.Clock()
