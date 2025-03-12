c7d50d0a3a3dc4bb653db9cf7708dd77aab84401
Netsfly <sasha.tsoi20@gmail.com>
2025-03-12 15:34:09 +0600

    fail check

--- snake.py
+++ snake.py
@@ -49,6 +49,7 @@ class MAIN:
     def update(self):
         self.snake.move_snake()
         self.check_collision()
+        self.check_fail()
     def draw_elements(self):
         self.fruit.draw_fruit()
         self.snake.draw_snake()
@@ -57,6 +58,16 @@ class MAIN:
         if self.fruit.pos == self.snake.body[0]:
             self.fruit.randomize()
             self.snake.add_block()
+            
+    def check_fail(self):
+        if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:
+            self.game_over()
+        
+            
+    def game_over(self):
+        pygame.quit()
+        sys.exit()       
+    
 pygame.init()
 cell_size = 40
 cell_number = 20
